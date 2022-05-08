0582b13a6b074715a697951aef0757047bcb3a00
takshch <takshch089@gmail.com>
2022-05-08 07:18:30 +0530

    Fix image optimiation error

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/website/' : ''
+  images: {
+    loader: "imgix",
+    path: "",
+  },
 }
 
 module.exports = nextConfig
