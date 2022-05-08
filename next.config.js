305d5d9909708e2bd14c8ad201350df6160a278e
takshch <takshch089@gmail.com>
2022-05-08 07:36:25 +0530

    Adds basePath to next.config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: isProd ? '/website' : '',
   assetPrefix: isProd ? '/website/' : '',
   images: {
     loader: "imgix",
