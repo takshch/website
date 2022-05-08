adf024e8e4c6760768da80a9ee6214bd5dfceaff
takshch <takshch089@gmail.com>
2022-05-08 07:23:23 +0530

    Fixes unresolved css path

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/website/' : '',
   images: {
     loader: "imgix",
     path: "",
